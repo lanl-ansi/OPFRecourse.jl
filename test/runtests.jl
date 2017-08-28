a354bb5e9110e1a28b5c31c6978144e7aa47decd
Yeesian Ng <ngyeesian@gmail.com>
2017-08-28 19:08:34 -0400

    Basis recourse (#5)
    * reshuffle order of constraints
    
    makes it easier to extract the basis matrix
    
    * initial working example
    
    * move basis code into its own file
    
    * add test requirements
    
    * add some code documentation
    
    * remove old assertions
    
    it seems you can’t depend on `m.model.colNames` to have meaningful
    basenames
    
    * convert some example code to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,17 @@
-using OPFRecourse, Gurobi, Base.Test
+using OPFRecourse, Gurobi, Distributions, Base.Test
 
 data_file = string(Pkg.dir(),"/AlternatingOPF/test/data/nesta_case30_ieee_prob.m")
 @time ref = PowerModels.build_ref(PowerModels.parse_file(data_file));
-for (i,u) in ref[:uncertainty]; u["std"] /= 50.0 end # scale down volatility
+for (i,u) in ref[:uncertainty]; u["std"] /= 100.0 end # scale down volatility
 ref[:branch][2]["rate_a"] = 0.83 # line tightening
 ref = OPFRecourse.NetworkReference(ref, bus_prob = 0.95, line_prob = 0.95)
 @time ccopf = OPFRecourse.ChanceConstrainedOPF(ref, Gurobi.GurobiSolver());
 @time JuMP.solve(ccopf.model, method=:Reformulate)
-
 @time m = OPFRecourse.SingleScenarioOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
 @time JuMP.solve(m.model)
 
 ω = Distributions.MvNormal([ref.refω[i]["mean"] for i in 1:ref.nuncertain], Array(ref.sqrtΣ^2))
-nsamples = 20_000
+nsamples = 1000
 ωsamples = rand(ω, nsamples)
 status = Array{Symbol}(nsamples);
 soln_p = zeros(nsamples, ref.ngen);
@@ -46,3 +45,17 @@ end
 
 sample_p = Float32.(soln_p[status .== :Optimal, :])
 sample_ω = Float32.(ωsamples[:,status .== :Optimal])'
+
+@test length(collect(keys(bases))) == 1
+@test length(collect(keys(cbases))) == 1
+@test length(collect(keys(rbases))) == 1
+OPFRecourse.get_opf_solution(m, ωsamples[:,1])
+cbasis, rbasis = MathProgBase.getbasis(m.model.internalModel)
+br = OPFRecourse.BasisRecourse(ref, m, cbasis, rbasis);
+for i in 1:noptimal
+    @test isapprox(
+        OPFRecourse.get_opf_solution(br, sample_ω[i,:]),
+        sample_p[i,:]
+    )
+end
+
