b45a31a33eb270163001d252a130efc4d96e843a
Yeesian Ng <ngyeesian@gmail.com>
2017-08-29 14:54:03 -0400

    implement projection method (#12)

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,7 +20,10 @@ scenarios = OPFRecourse.OPFScenarios(ref, m, nsamples = 1000)
 br = OPFRecourse.BasisRecourse(ref, m, scenarios.cbases[1], scenarios.rbases[1]);
 for i in 1:size(scenarios.scenarios,1)
     @test isapprox(
-        OPFRecourse.get_opf_solution(br, scenarios.scenarios[i,:]),
+        OPFRecourse.reproject(
+            ref,
+            OPFRecourse.get_opf_solution(br, scenarios.scenarios[i,:])
+        ),
         scenarios.solutions[i,:],
         atol = 1e-6
     )
