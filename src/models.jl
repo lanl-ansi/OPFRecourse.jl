aa24fb54a14ae08abc4e8ffc6cd4d91fba33c530
Yeesian Ng <ngyeesian@gmail.com>
2017-09-01 10:23:17 -0400

    Improved model construction (#22)
    * compute single line flows
    
    * alleviates some problems during the model construction phase
    
    by (i) caching some of the bus values and (ii) avoiding the
    construction of unnecessary jump expressions in the line flow
    constraints

--- src/models.jl
+++ src/models.jl
@@ -1,5 +1,4 @@
-"computes (p + μ + ω - d)"
-function busfromRHS(ref, p, ω)
+function singlelineflow(ref, p, ω, l)
     function busvalue(i)
         result = ω[i] - ref.bus[i].pd - ref.bus[i].gs
         if !isempty(ref.bus[i].gens)
@@ -7,11 +6,11 @@ function busfromRHS(ref, p, ω)
         end
         result
     end
-    Any[busvalue(i) for i in 1:ref.nbus]
+    θ(i) = sum(ref.π[i,j]*busvalue(j) for j in 1:ref.nbus)
+    ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
 end
 
-"computes (p - α⋅ω + μ + ω - d) with aggregated affine recourse"
-function busfromRHS(ref, p, α, ω)
+function singlelineflow(ref, p, α, ω, l)
     function busvalue(i)
         result = ω[i] - ref.bus[i].pd - ref.bus[i].gs
         if !isempty(ref.bus[i].gens)
@@ -19,11 +18,11 @@ function busfromRHS(ref, p, α, ω)
         end
         result
     end
-    Any[busvalue(i) for i in 1:ref.nbus]
+    θ(i) = sum(ref.π[i,j]*busvalue(j) for j in 1:ref.nbus)
+    ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
 end
 
-"computes (p - α⋅ω + μ + ω - d) with full affine recourse"
-function fullbusfromRHS(ref, p, α, ω)
+function singlelineflow2(ref, p, α, ω, l)
     function busvalue(i)
         result = ω[i] - ref.bus[i].pd - ref.bus[i].gs
         if !isempty(ref.bus[i].gens)
@@ -34,15 +33,16 @@ function fullbusfromRHS(ref, p, α, ω)
         end
         result
     end
-    Any[busvalue(i) for i in 1:ref.nbus]
+    θ(i) = sum(ref.π[i,j]*busvalue(j) for j in 1:ref.nbus)
+    ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
 end
 
-"computes B_f*θ where B_f is the matrix of power transfer factors"
-Bftheta(ref, θ) = [l.β*(θ[l.frombus] - θ[l.tobus]) for l in ref.line]
-
-lineflow(ref, p, ω)    = Bftheta(ref, ref.π*busfromRHS(ref, p, ω))
-lineflow(ref, p, α, ω) = Bftheta(ref, ref.π*busfromRHS(ref, p, α, ω))
-fulllineflow(ref, p, α, ω) = Bftheta(ref, ref.π*fullbusfromRHS(ref, p, α, ω))
+"computes B_f*inv(B̃)(p + μ + ω - d)"
+lineflow(ref, p, ω) = [singlelineflow(ref, p, ω, l) for l in 1:ref.nline]
+"computes B_f*inv(B̃)(p - α⋅ω + μ + ω - d) with aggregated affine recourse"
+lineflow(ref, p, α, ω) = [singlelineflow(ref, p, α, ω, l) for l in 1:ref.nline]
+"computes B_f*inv(B̃)(p - α⋅ω + μ + ω - d) with full affine recourse"
+fulllineflow(ref, p, α, ω) = [singlelineflow2(ref, p, α, ω, l) for l in 1:ref.nline]
 
 mutable struct ChanceConstrainedOPF
     model::JuMP.Model
@@ -58,7 +58,16 @@ function ChanceConstrainedOPF(
     JuMP.@variable(model, ref.gen[i].pmin <= p[i in 1:ref.ngen] <= ref.gen[i].pmax, start=ref.gen[i].pstart)
     JuMP.@variable(model,                    α[i in 1:ref.ngen] >= 0)
     JuMPChance.@indepnormal(model,           ω[j in 1:ref.nbus], mean=0, var=ref.stdω[j]^2)
-    f = lineflow(ref, p, α, ω)
+    function busvalue(i)
+        result = ω[i] - ref.bus[i].pd - ref.bus[i].gs
+        if !isempty(ref.bus[i].gens)
+            result += sum(p[g] - α[g]*sum(ω) for g in ref.bus[i].gens)
+        end
+        result
+    end
+    busvalue = [busvalue(i) for i in 1:ref.nbus]
+    θ(i) = sum(ref.π[i,j]*busvalue[j] for j in 1:ref.nbus)
+    lineflow(l) = ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
     JuMP.@constraints model begin
         sum(α[i] for i in 1:ref.ngen) == 1
         sum(α[g] for g in ref.bus[ref.r].gens) == 0
@@ -69,8 +78,8 @@ function ChanceConstrainedOPF(
         JuMP.@constraint(model, p[i] - sum(ω)*α[i] >= ref.gen[i].pmin, with_probability=ref.bus_prob)
     end
     for l in 1:ref.nline
-        JuMP.@constraint(model, f[l] <= ref.line[l].rate, with_probability=ref.line_prob)
-        JuMP.@constraint(model, f[l] >= -ref.line[l].rate, with_probability=ref.line_prob)
+        JuMP.@constraint(model, lineflow(l) <= ref.line[l].rate, with_probability=ref.line_prob)
+        JuMP.@constraint(model, lineflow(l) >= -ref.line[l].rate, with_probability=ref.line_prob)
     end
     JuMP.@objective(model, Min, cost(ref, p))
     ChanceConstrainedOPF(model,p,α)
@@ -100,7 +109,19 @@ function FullChanceConstrainedOPF(
     JuMP.@variable(model, ref.gen[i].pmin <= p[i in 1:ref.ngen] <= ref.gen[i].pmax, start=ref.gen[i].pstart)
     JuMP.@variable(model,                    α[i in 1:ref.ngen, j in 1:ref.nbus] >= 0)
     JuMPChance.@indepnormal(model,           ω[j in 1:ref.nbus], mean=0, var=ref.stdω[j]^2)
-    f = fulllineflow(ref, p, α, ω)
+    function busvalue(i)
+        result = ω[i] - ref.bus[i].pd - ref.bus[i].gs
+        if !isempty(ref.bus[i].gens)
+            result += sum(
+                p[g] - sum(α[g,j]*ω[j] for j in 1:ref.nbus)
+                for g in ref.bus[i].gens
+            )
+        end
+        result
+    end
+    busvalue = [busvalue(i) for i in 1:ref.nbus]
+    θ(i) = sum(ref.π[i,j]*busvalue[j] for j in 1:ref.nbus)
+    lineflow(l) = ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
     JuMP.@constraints model begin
         [j in 1:ref.nbus], sum(α[i,j] for i in 1:ref.ngen) == 1
         powerbalance, 0 == sum(sum(p[g] for g in b.gens) - b.pd - b.gs for b in ref.bus)
@@ -110,8 +131,8 @@ function FullChanceConstrainedOPF(
         JuMP.@constraint(model, p[i] - sum(α[i,j]*ω[j] for j in 1:ref.nbus) >= ref.gen[i].pmin, with_probability=ref.bus_prob)
     end
     for l in 1:ref.nline
-        JuMP.@constraint(model, f[l] <= ref.line[l].rate, with_probability=ref.line_prob)
-        JuMP.@constraint(model, f[l] >= -ref.line[l].rate, with_probability=ref.line_prob)
+        JuMP.@constraint(model, lineflow(l) <= ref.line[l].rate, with_probability=ref.line_prob)
+        JuMP.@constraint(model, lineflow(l) >= -ref.line[l].rate, with_probability=ref.line_prob)
     end
     JuMP.@objective(model, Min, cost(ref, p))
     FullChanceConstrainedOPF(model,p,α)
@@ -134,10 +155,14 @@ function SingleScenarioOPF(
     model = JuMP.Model(solver=solver)
     JuMP.@variable(model, ref.gen[i].pmin <= p[i in 1:ref.ngen] <= ref.gen[i].pmax, start=ref.gen[i].pstart)
     JuMP.@variable(model,                    ω[i in 1:ref.nbus])
-    f = lineflow(ref, p, ω)
+    JuMP.@expression(model, busvalue[i in 1:ref.nbus],
+        sum(p[g] for g in ref.bus[i].gens) + ω[i] - ref.bus[i].pd - ref.bus[i].gs 
+    )
+    θ(i) = sum(ref.π[i,j]*busvalue[j] for j in 1:ref.nbus)
+    lineflow(l) = ref.line[l].β*(θ(ref.line[l].frombus) - θ(ref.line[l].tobus))
     JuMP.@constraints model begin
-        [l in 1:ref.nline], f[l] <= ref.line[l].rate
-        [l in 1:ref.nline], f[l] >= -ref.line[l].rate
+        [l in 1:ref.nline], lineflow(l) <= ref.line[l].rate
+        [l in 1:ref.nline], lineflow(l) >= -ref.line[l].rate
         0 == sum(sum(p[g] for g in ref.bus[i].gens) + ω[i] - ref.bus[i].pd - ref.bus[i].gs
                  for i in 1:ref.nbus)
     end
