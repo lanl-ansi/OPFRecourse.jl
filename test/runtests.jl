a318a1423dd92091ab982f2c1845910fdeb032dd
Yeesian Ng <ngyeesian@gmail.com>
2017-12-20 17:02:25 -0500

    switch to Clp for tests
    closes #9

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,14 @@
-using OPFRecourse, Gurobi, Base.Test
+using JuMP, OPFRecourse, Clp, Base.Test
 
 data_file = string(Pkg.dir(),"/OPFRecourse/test/data/nesta_case30_ieee.m")
 
 @testset "Lower Volatility (Only 1 Optimal Basis)" begin
     @time ref = OPFRecourse.NetworkReference(data_file, bus_prob = 0.95, line_prob = 0.95, σscaling = 0.05);
-    
-    @time ccopf = OPFRecourse.ChanceConstrainedOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
-    @time JuMP.solve(ccopf.model, method=:Reformulate)
 
-    @time fullccopf = OPFRecourse.FullChanceConstrainedOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
-    @time JuMP.solve(fullccopf.model, method=:Reformulate)
-
-    @time m = OPFRecourse.SingleScenarioOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
+    @time m = OPFRecourse.SingleScenarioOPF(ref, Clp.ClpSolver());
     @time JuMP.solve(m.model)
 
     @time scenarios = OPFRecourse.OPFScenarios(ref, m, nsamples = 1000);
-    @test length(OPFRecourse.get_opf_solution(ccopf, scenarios.scenarios[1,:])) == ref.ngen
-    @test length(OPFRecourse.get_opf_solution(fullccopf, scenarios.scenarios[1,:])) == ref.ngen
     @test length(OPFRecourse.get_opf_solution(m, scenarios.scenarios[1,:])) == ref.ngen
 
     br = OPFRecourse.BasisRecourse(ref, m, scenarios.cbases[1], scenarios.rbases[1]);
@@ -38,11 +30,8 @@ end
 
 @testset "Higher Volatility (With 2 Optimal Basis)" begin
     @time ref = OPFRecourse.NetworkReference(data_file, bus_prob = 0.6, line_prob = 0.6, σscaling = 0.5);
-    
-    @time fullccopf = OPFRecourse.FullChanceConstrainedOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
-    @time JuMP.solve(fullccopf.model, method=:Reformulate)
 
-    @time m = OPFRecourse.SingleScenarioOPF(ref, Gurobi.GurobiSolver(OutputFlag=0));
+    @time m = OPFRecourse.SingleScenarioOPF(ref, Clp.ClpSolver());
     @time JuMP.solve(m.model)
     
     srand(1234)
@@ -60,7 +49,7 @@ end
         for i in 1:size(uniquebases,1)
     ]
 
-    ensemble = OPFRecourse.EnsembleRecourse(ref, fullccopf, basisrecourses)
+    ensemble = OPFRecourse.EnsembleRecourse(ref, basisrecourses[1], basisrecourses[2:end])
     for i in 1:size(scenarios.scenarios,1)
         @test isapprox(
             OPFRecourse.get_opf_solution(ensemble, scenarios.scenarios[i,:]),
